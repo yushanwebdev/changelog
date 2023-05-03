60349b5f05af292499f1cdf317420a083f88b758
yushanwebdev <yushanwebdev@gmail.com>
2023-05-03 23:49:50 +0530

    feat: Routes & Middleware - Creating a Custom Middleware

--- src/router.ts
+++ src/router.ts
@@ -1,36 +1,37 @@
 import { Router } from "express";
+import { CustomRequest } from "./types";
 
 const router = Router();
 
 /*
  * Product
  */
-router.get('/product', (req, res) => {
-    res.json({
-        message: 'hello'
-    })
+router.get("/product", (req, res) => {
+  res.json({
+    message: "Hello",
+  });
 });
-router.get('/product/:id', () => {});
-router.put('/product/:id', () => {});
-router.post('/product', () => {});
-router.delete('/product/:id', () => {})
+router.get("/product/:id", () => {});
+router.put("/product/:id", () => {});
+router.post("/product", () => {});
+router.delete("/product/:id", () => {});
 
 /*
  * Update
  */
-router.get('/update', () => {});
-router.get('/update/:id', () => {});
-router.put('/update/:id', () => {});
-router.post('/update', () => {});
-router.delete('/update/:id', () => {})
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.put("/update/:id", () => {});
+router.post("/update", () => {});
+router.delete("/update/:id", () => {});
 
 /*
  * Update Points
  */
-router.get('/updatepoint', () => {});
-router.get('/updatepoint/:id', () => {});
-router.put('/updatepoint/:id', () => {});
-router.post('/updatepoint', () => {});
-router.delete('/updatepoint/:id', () => {})
+router.get("/updatepoint", () => {});
+router.get("/updatepoint/:id", () => {});
+router.put("/updatepoint/:id", () => {});
+router.post("/updatepoint", () => {});
+router.delete("/updatepoint/:id", () => {});
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,30 @@
-import express from 'express'
-import router from './router';
-import morgan from 'morgan'
+import express, { NextFunction, Request, Response } from "express";
+import router from "./router";
+import morgan from "morgan";
+import { CustomRequest } from "./types";
 
 const app = express();
 
-app.use(morgan('dev'))
+const customLogger =
+  (message: string) => (_req: Request, _res: Response, next: NextFunction) => {
+    console.log(`Hello from ${message}`);
+    next();
+  };
 
-app.get("/", (req, res) => {
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(customLogger("Yushan"));
+
+// app.use((req, res, next) => {
+//   (req as CustomRequest).shhhh_secret = "doggy";
+//   next();
+
+//   res.status(401);
+//   res.send("Nope");
+// });
+
+app.get("/", (_req, res) => {
   console.log("hello from Express");
   res.status(200);
   res.json({
@@ -14,6 +32,6 @@ app.get("/", (req, res) => {
   });
 });
 
-app.use('/api', router)
+app.use("/api", router);
 
-export default app
+export default app;
