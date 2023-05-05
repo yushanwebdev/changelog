52c3be6cb1771b0639a054e8155688718ff9a5df
yushanwebdev <yushanwebdev@gmail.com>
2023-05-05 23:17:48 +0530

    feat: Route & Error Handlers - Adding Validation to Routes

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
@@ -11,7 +12,16 @@ router.get("/product", (req, res) => {
   });
 });
 router.get("/product/:id", () => {});
-router.put("/product/:id", () => {});
+router.put("/product/:id", body("name").isString(), (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400);
+    res.json({
+      errors: errors.array(),
+    });
+  }
+});
 router.post("/product", () => {});
 router.delete("/product/:id", () => {});
 
