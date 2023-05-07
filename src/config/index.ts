222b363102cae31748b9227cce5684158ca2999b
yushanwebdev <yushanwebdev@gmail.com>
2023-05-07 20:31:51 +0530

    feat: Config, Performance & Testing - Creating Environment Configurations

---
+++ src/config/index.ts
@@ -0,0 +1,27 @@
+import merge from "lodash.merge";
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const stage = process.env.STAGE || "local";
+
+let envConfig;
+
+if (stage === "production") {
+  envConfig = require("./prod").default;
+} else if (stage === "testing") {
+  envConfig = require("./test").default;
+} else {
+  envConfig = require("./local").default;
+}
+
+const defaultConfig = {
+  stage,
+  env: process.env.NODE_ENV,
+  port: 3001,
+  secrets: {
+    jwt: process.env.JWT_SECRET,
+    dbUrl: process.env.DB_URL,
+  },
+};
+
+export default merge(defaultConfig, envConfig);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
+import config from "./config";
 import app from "./server";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-app.listen(3001, () => {
-  console.log("hello on http://localhost:3001");
+app.listen(config.port, () => {
+  console.log(`hello on http://localhost:${config.port}`);
 });
 
 process.on("uncaughtException", (err) => {
